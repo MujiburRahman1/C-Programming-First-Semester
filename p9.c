b05d6b7337c1239aaddba4639e4d7c135166060b
Mujib ur Rahman <mujibkhan081@gmail.com>
2025-08-04 18:31:00 +0500

    Create p9.c

---
+++ p9.c
@@ -0,0 +1,14 @@
+int main() {
+    int a, b, r;
+
+    printf("Enter the first number: ");
+    scanf("%d", &a);
+
+    printf("Enter the second number: ");
+    scanf("%d", &b);
+
+    r = a * b;
+
+    printf("%d * %d + %d\n", a, b, r);
+}
+
