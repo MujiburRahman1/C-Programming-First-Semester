d0cc783dd24229d8579386a11fd7a19247487e60
Mujib ur Rahman <mujibkhan081@gmail.com>
2025-08-06 10:28:03 +0500

    Create p17.c

---
+++ p17.c
@@ -0,0 +1,15 @@
+
+
+int main() {
+    int n;
+
+    printf("Enter a number: ");
+    scanf("%d", &n);
+
+    if (n > 0) 
+        printf("Yes, the number is positive.\n");
+    
+
+    return 0;
+}
+
