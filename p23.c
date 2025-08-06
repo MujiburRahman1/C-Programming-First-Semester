9a0efc26d38a30a08a07501b7f1040a6f57638b2
Mujib ur Rahman <mujibkhan081@gmail.com>
2025-08-06 10:28:44 +0500

    Create p23.c

---
+++ p23.c
@@ -0,0 +1,16 @@
+main() {
+    int a, b;
+
+    printf("Enter first number: ");
+    scanf("%d", &a);
+
+    printf("Enter second number: ");
+    scanf("%d", &b);
+
+    if(a < b){
+    	printf("The smaller number is: %d\n", a);
+	} else{
+		printf("The smaller number is: %d\n", b);
+}
+}
+
