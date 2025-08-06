671ac928b35574193bcf27a9398aa449abdb50e2
Mujib ur Rahman <mujibkhan081@gmail.com>
2025-08-06 10:28:30 +0500

    Create p21.c

---
+++ p21.c
@@ -0,0 +1,19 @@
+main()
+{
+int a,b;
+printf("Please Enter a Value\n");
+scanf("%d",&a);
+b=a%2;
+if(b==0)
+{
+printf("This is even number\n");
+}
+else
+{
+printf("This is odd number");
+}
+printf("\nGOOD BYE");
+}
+
+  
+
