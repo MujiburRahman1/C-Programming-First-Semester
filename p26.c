4a6bfbedf2cf9d44ea10eb7d9d28d0f2f3defdbb
Mujib ur Rahman <mujibkhan081@gmail.com>
2025-08-10 10:09:18 +0500

    Create p26.c

---
+++ p26.c
@@ -0,0 +1,17 @@
+main() {
+    int n1, n2, n3;
+
+    printf("Enter three numbers: ");
+    scanf("%d %d %d", &n1, &n2, &n3);
+
+    if (n1 > n2 && n1 > n3) {
+        printf("The largest number is: %d\n", n1);
+    } else if (n2 > n1 && n2 > n3) {
+        printf("The largest number is: %d\n", n2);
+    } else if (n3 > n1 && n3 > n2) {
+        printf("The largest number is: %d\n", n3);
+    }
+
+   
+}
+
