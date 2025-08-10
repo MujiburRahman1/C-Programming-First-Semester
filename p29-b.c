1bda7a658e5c45a30178e353d0765ae20278dfd1
Mujib ur Rahman <mujibkhan081@gmail.com>
2025-08-10 10:10:02 +0500

    Create p29-b.c

---
+++ p29-b.c
@@ -0,0 +1,27 @@
+main() {
+int a, b, c;
+char ch;
+
+printf("Enter two numbers: ");
+scanf("%d %d", &a, &b);
+
+printf("Enter +, - or *: ");
+ 
+ch = getche();
+
+if (ch == '+') {
+c = a + b;
+printf("\n%d %c %d = %d", a, ch, b, c);} 
+else if (ch == '-') {
+c = a - b;
+printf("\n%d %c %d = %d", a, ch, b, c);} 
+else if (ch == '*') {
+c = a * b;
+printf("\n%d %c %d = %d", a, ch, b, c);} 
+else {
+printf("Invalid operator");
+    }
+
+    return 0;
+}
+
